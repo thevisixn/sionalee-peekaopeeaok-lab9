2e1b7f5207dcb5bfa789024470ceec8d78cd5d6d
visionalee <flook.xtnt@gmail.com>
2025-12-19 16:08:18 +0700

    lab9_2 completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,22 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int n, int m){
+	if (n > 0 && m > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < m; j++)
+			{
+				cout << "O";
+			}
+			cout << "\n";
+		}
+		
+	} else {
+		cout << "Invalid input";
+	}
+}
 
 int main(){
 	
